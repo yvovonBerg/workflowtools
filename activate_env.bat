166ffc71267126a8b8aa41006658b771cbf5b160
Yvo von Berg <yvo_von_berg@hotmail.com>
2019-01-13 17:07:29 +0000

    added window title to launch script of timemanager

--- activate_env.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set setting_file = "%cd%/.vscode/settings.json"
-FOR /F "tokens=* USEBACKQ" %%F IN (`python "U:\tools\internal\workflowtools\read_settings.py" "%cd%"`) DO (
-SET var=%%F
-)
-echo %var%
--- tm.bat
+++ tm.bat
@@ -1,5 +1,5 @@
 @echo off
+TITLE "TimeManager"
 PUSHD %CD%
 cd /D "U:\tools\internal\workflowtools"
-dev.bat && python "U:\tools\internal\workflowtools\time_manager\time_manager\time_manager.py" %* && q && POPD
-
+dev.bat && python "U:\tools\internal\workflowtools\time_manager\time_manager\time_manager.py" %* && q && POPD && TITLE Command Prompt
